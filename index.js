6f64d81d1c58eac9fce1d2cb50da72b15a1fc5dd
Abel McElroy <abel.h.mcelroy@gmail.com>
2023-07-09 12:09:51 -0400

    cors

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const server = express();
+const cors = require('cors')
 const knex = require("./knexConfig");
+
+server.use(cors({
+  "origin": process.env.CORS_ALLOWED_ORIGIN,
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204,
+  "credentials": true, // only for cookies
+}));
 
 server.get("/", async (_, res) => {
   // await knex('users').insert({ full_name: "Abel" })
   console.log("it worked")
   res.send("IT WORKS!");
-})
+});
 
 server.patch("/", (_, res) => {
   console.log("this works too")
   res.json({ rand: Math.random() });
-})
+});
 
 server.listen(3001);
